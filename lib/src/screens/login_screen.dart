5d8d7936f8a728aedc3b9e8f99e5b86b4339b8a8
Ansh Gujral <anshgujral1997@gmail.com>
2018-12-23 22:19:44 +0530

    added email and password field
    Signed-off-by: Ansh Gujral <anshgujral1997@gmail.com>

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+    State<StatefulWidget> createState() {
+      // TODO: implement createState
+      return LoginScreenState();
+    }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+    Widget build(context) {
+      return Container(
+        margin: EdgeInsets.all(30.0),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              emailField(),
+              passwordField(),
+              // submitButton(),
+            ],
+          ),
+        ),
+      );
+    }
+
+    Widget emailField() {
+      return TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          labelText: 'Email',
+          hintText: 'you@example.com',
+        ),
+      );
+    }
+
+    Widget passwordField() {
+      return TextFormField(
+        obscureText: true,
+        decoration: InputDecoration(
+          labelText: 'Password',
+          hintText:  'Password...'
+        ),
+      );
+    }
+
+    Widget submitButton() {
+
+    }
+
+}
