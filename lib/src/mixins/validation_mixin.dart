5c10d1a1fe511852dcf155be7290367a7268b652
Ansh Gujral <anshgujral1997@gmail.com>
2019-01-10 20:46:08 +0530

    fixed mixin bug and some code cleaning
    Signed-off-by: Ansh Gujral <anshgujral1997@gmail.com>

--- lib/src/mixins/validation_mixin.dart
+++ lib/src/mixins/validation_mixin.dart
@@ -1,14 +1,14 @@
 class ValidationMixin {
-  String validateEmail(String value) {
+  String validatePassword(String value) {
     if (value.length < 4) {
       return 'Password should be less than 4 characters';
     }
     return null;
   }
 
-  String validatePassword(String value) {
+  String validateEmail(String value) {
     // return if valid otherwise string showing error message
-    if(!value.contains('@')) {
+    if (!value.contains('@')) {
       return 'Please enter a valid email';
     }
     return null;
--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -3,74 +3,73 @@ import '../mixins/validation_mixin.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
-    State<StatefulWidget> createState() {
-      // TODO: implement createState
-      return LoginScreenState();
-    }
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return LoginScreenState();
+  }
 }
 
-class LoginScreenState extends State<LoginScreen> with ValidationMixin{
+class LoginScreenState extends State<LoginScreen> with ValidationMixin {
   final formKey = GlobalKey<FormState>();
 
   String email = '';
   String password = '';
 
-    Widget build(context) {
-      return Container(
-        margin: EdgeInsets.all(30.0),
-        child: Form(
-          key: formKey,
-          child: Column(
-            children: <Widget>[
-              emailField(),
-              passwordField(),
-              Container(margin: EdgeInsets.only(top:  25.0),),
-              submitButton(),
-            ],
-          ),
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(30.0),
+      child: Form(
+        key: formKey,
+        child: Column(
+          children: <Widget>[
+            emailField(),
+            passwordField(),
+            Container(
+              margin: EdgeInsets.only(top: 25.0),
+            ),
+            submitButton(),
+          ],
         ),
-      );
-    }
+      ),
+    );
+  }
 
-    Widget emailField() {
-      return TextFormField(
-        keyboardType: TextInputType.emailAddress,
-        decoration: InputDecoration(
-          labelText: 'Email',
-          hintText: 'you@example.com',
-        ),
-        validator: validateEmail,
-        onSaved: (String value) {
-          email = value;
-        },
-      );
-    }
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Email',
+        hintText: 'you@example.com',
+      ),
+      validator: validateEmail,
+      onSaved: (String value) {
+        email = value;
+      },
+    );
+  }
 
-    Widget passwordField() {
-      return TextFormField(
-        obscureText: true,
-        decoration: InputDecoration(
-          labelText: 'Password',
-          hintText:  'Password...'
-        ),
-        validator: validatePassword,
-        onSaved: (String value) {
-          password = value;
-        },
-      );
-    }
+  Widget passwordField() {
+    return TextFormField(
+      obscureText: true,
+      decoration:
+          InputDecoration(labelText: 'Password', hintText: 'Password...'),
+      validator: validatePassword,
+      onSaved: (String value) {
+        password = value;
+      },
+    );
+  }
 
-    Widget submitButton() {
-      return RaisedButton(
-        color: Colors.blue,
-        onPressed: () {
-          if(formKey.currentState.validate()) {
-            formKey.currentState.save();
-            print('Time to post $email and $password to my API');
-          }
-        },
-        child: Text('Submit'),
-      );
-    }
-
+  Widget submitButton() {
+    return RaisedButton(
+      color: Colors.blue,
+      onPressed: () {
+        if (formKey.currentState.validate()) {
+          formKey.currentState.save();
+          print('Time to post $email and $password to my API');
+        }
+      },
+      child: Text('Submit'),
+    );
+  }
 }
